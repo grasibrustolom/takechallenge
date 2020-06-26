918ef6ca5c5e4bd242a024d7aa7d2a8e512c6839
GraziBrustolom <graziele_brustolom@hotmail.com>
2020-06-25 22:56:44 -0300

    router update

--- routes/repositories.js
+++ routes/repositories.js
@@ -1,40 +1,7 @@
-const express = require('express');
-const app = require('../app');
-const axios = require('axios');
+const express = require("express");
 const router = express.Router();
+const getReposController = require("../controllers/repositories");
 
-router.get('/',(req,res,next) => {
-
- 
-  axios.get("https://api.github.com/orgs/takenet/repos?sort=created&direction=asc").then(function (response) {
-    // handle success
-
-    let repositories = response.data;
-
-    repositories = repositories.filter((repoCSharp) => { return repoCSharp.language === 'C#'; }).splice(0,5);
-
-    res.status(200).send({
-      first: repositories[0],
-      second: repositories[1],
-      third: repositories[2],
-      fourth: repositories[3],
-      fifth: repositories[4]
-    })
-    
-  })
-  .catch(function (error) {
-    // handle error
-    res.send({
-        sucess: false,
-        result: error
-     })
-  })
-  .finally(function () {
-    // always executed
-  });
-
-
-}); 
-
+router.get("/", getReposController);
 
 module.exports = router;
